92e62041b42210256d9f497fa1b397b74b863176
Raevan Gregson <Raevan@Raevans-MBP.netgear.com>
2017-10-30 22:06:07 -0400

    prompt exit added

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
 
 $(document).ready(function(){
+
+  var elementClicked = "";
   $(".circle").click(function(){
     if($( this ).hasClass( "available"))
     {
       $(".prompt").css("display","flex");
+      elementClicked = $(this);
   }
 });
+
+
   $("button").click(function(){
     if($(".circle.available")){
-      $(".circle").removeClass('available').addClass('reserved');
+      $(elementClicked).removeClass('available').addClass('reserved');
     $(".prompt").hide();
   }
   });
+
+$("img").click(function(){
+  $(".prompt").hide();
+});
+
+
+
+
+
+
 });
